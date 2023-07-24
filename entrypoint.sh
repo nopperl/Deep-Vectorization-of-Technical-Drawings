881e6a12af13222cbc43c196b2b95bc6831c7733
nopperl <54780682+nopperl@users.noreply.github.com>
2023-07-24 14:06:02 +0200

    fix aspect ratio script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-apt-get update && apt-get install -y imagemagick
+apt-get update && apt-get install -y imagemagick time
 input_dir=/code/Deep-Vectorization-of-Technical-Drawings/data
 cd /code/Deep-Vectorization-of-Technical-Drawings
 ./download_model.sh
 . /opt/.venv/vect-env/bin/activate
 find $input_dir -type f -name '*.png' -exec basename {} \; | xargs -L1 -I{} ./test_lineart.sh {}
-./fix_aspect_ratio.sh
+find logs/outputs/vectorization/lines/merging_output -maxdepth 1 -name '*.svg' -exec ./fix_aspect_ratio.sh {} \;
--- fix_aspect_ratio.sh
+++ fix_aspect_ratio.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-input_dir="./data"
-output_dir="logs/outputs/vectorization/lines/merging_output"
-
-for input_image in "$input_dir"/*; do
-	filename=$(basename $input_image)
-	svg_image=$output_dir/$filename
-	svg_image=${svg_image/png/svg}
-	echo $svg_image
-	width=$(identify -format '%w' $input_image)
-	height=$(identify -format '%h' $input_image)
+input_dir="data"
+svg_image=$1
+svg_image_name=$(basename "$svg_image")
+input_image="$input_dir/${svg_image_name/svg/png}"
+echo $svg_image_name
+echo $input_image
+if [ -f "$input_image" ]; then
+	echo $input_image
+	width=$(identify -format '%w' "$input_image")
+	height=$(identify -format '%h' "$input_image")
 	svg_def='<svg xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink" baseProfile="full" height="'$height'px" version="1.1" viewBox="0 0 '$width' '$height'" width="'$width'px">'
-	if [ -f $svg_image ]; then
-		echo $svg_image
-		sub_cmd="2s|.*|$svg_def|"
-		sed -i "$sub_cmd" $svg_image
-	fi
-done
+	sub_cmd="2s|.*|$svg_def|"
+	sed -i "$sub_cmd" "$svg_image"
+fi
