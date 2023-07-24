77df22ecf9de4e52fba0e8424d0be84821546ce8
nopperl <54780682+nopperl@users.noreply.github.com>
2023-07-24 14:05:36 +0200

    add script to measure runtime

--- test_lineart.sh
+++ test_lineart.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 filename=$1
-python run_pipeline.py --primitive_type curve --gpu 0 --data_dir /data/ --json_path /code/Deep-Vectorization-of-Technical-Drawings/vectorization/models/specs/resnet18_blocks1_bn_64__c2h__trans_heads4_feat256_blocks8_ffmaps512__h2o__out512.json --model_path /logs/vectorization/curves/model_curves.weights --image_name $filename
+mkdir -p /logs/outputs/runtime/
+time -o /logs/outputs/runtime/"${filename%png}txt" python run_pipeline.py --primitive_type curve --gpu 0 --data_dir /data/ --json_path /code/Deep-Vectorization-of-Technical-Drawings/vectorization/models/specs/resnet18_blocks1_bn_64__c2h__trans_heads4_feat256_blocks8_ffmaps512__h2o__out512.json --model_path /logs/vectorization/curves/model_curves.weights --image_name "$filename"
